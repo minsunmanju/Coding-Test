93bf6e84bf387a1faf8bb00a87732dbf6b3374d4
minsunmanju <157439430+minsunmanju@users.noreply.github.com>
2026-03-22 00:27:28 +0900

    [Bronze V] Title: 사칙연산, Time: 92 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [a,b] = input[0].split(" ").map(Number)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(Math.floor(a/b))
+console.log(a%b)
