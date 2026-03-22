5e61006895543ea842dc77af5307c0c543483fc6
minsunmanju <157439430+minsunmanju@users.noreply.github.com>
2026-03-22 22:08:07 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const T = Number(input[0])
+for(let i =1; i<=T; i++){
+    const [a,b] = input[i].split(" ").map(Number)
+    console.log(a+b)
+}
